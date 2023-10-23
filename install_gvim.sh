555b0618a3e8071e2a242a460b762322dd5230e0
Jiwon <zzwon1212@gmail.com>
2023-10-23 20:56:00 +0900

    fix: Fix .vimrc part

--- install_gvim.sh
+++ install_gvim.sh
@@ -11,9 +11,8 @@ cd ~/.vim/colors
 curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 
 # Make .vimrc
-echo -e '
-" clipboard settings : Copy/Paste/Cut
-if has('unnamedplus')
+echo -e '"clipboard settings : Copy/Paste/Cut
+if has("unnamedplus")
 	set clipboard=unnamed,unnamedplus
 endif
 
@@ -21,8 +20,7 @@ set nu
 set ai cindent ts=4 sw=4 hls
 " option to read files encoded by euc-kr
 set fencs=ucs-bom,utf-8,japan,korea,latin-1
-colorscheme jellybeans
-' >> .vimrc
+colorscheme jellybeans' >> ~/.vimrc
 
 # Verify installed versions
 vim --version | grep clipboard
