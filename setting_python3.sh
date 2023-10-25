3694e9c6979b000088d6af1903cbe2d44d96cf26
Jiwon Lee <zzwon1212@gmail.com>
2023-10-26 08:25:46 +0900

    fix: Change Python minor version

--- setting_python3.sh
+++ setting_python3.sh
@@ -2,7 +2,7 @@
 
 # Change mirror from default to kakao
 echo "Setting python3 as default"
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 # Verify python version
 echo "Python version:"
