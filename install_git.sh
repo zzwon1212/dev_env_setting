73a77f5b86674bf72c989b6f95a49ee7a45a20a9
Jiwon Lee <zzwon1212@gmail.com>
2023-10-26 01:13:31 +0900

    fix: Add -y command to install automatically

--- install_git.sh
+++ install_git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install necessary dependencies
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl
