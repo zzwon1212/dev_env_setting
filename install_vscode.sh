2643044a37ff7727d934909c1ec0a10adc7b9798
Jiwon Lee <zzwon1212@gmail.com>
2023-12-16 16:30:44 +0900

    Create install_vscode.sh

---
+++ install_vscode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install the repository and key
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Update the package cache
+sudo apt install apt-transport-https
+sudo apt update
+
+# Install Visual Studio Code
+echo "Installing Visual Stuido Code..."
+sudo apt install code # or code-insiders
+
+# Verify installed versions
+echo "Visual Studio Code version:"
+code --version
